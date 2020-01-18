a8d9a038c648a15c9d5c2114507f24d4cd6157b0
Jia Wei Kho <jwk33@cam.ac.uk>
2020-01-18 22:19:53 +0000

    Dictionary of each leg and mode times

--- getLocation.py
+++ getLocation.py
@@ -11,7 +11,7 @@ from google.auth.transport.requests import Request
 
 apiKey = 'AIzaSyA1HfqmvNY_qRHR_aV1JxDDcXKlQMWxuAo'
 
-travel_mode_time = {}
+
 
 def get_lat_lng(apiKey, address):
 
@@ -29,52 +29,64 @@ def get_lat_lng(apiKey, address):
         lng = 0
     return lat, lng
 
-def get_travel_time(apiKey,location_list,mode_selected): # mode = walking, driving, bicycling, transit
+def get_travel_time(apiKey,origin,destination,mode_selected): # mode = walking, driving, bicycling, transit
+    travel_mode_time = {}
     # car = 0.525 kg per hour
     # bus = 0.822 g per km
     import requests
     time = 0
     carbon_footprint = 0
-    for i in range(len(location_list)-1):
+    # url = ('https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origin={}&destination={}&mode={}&key={}'
+    #         #        .format(origin.replace(' ','+'),
+    #         #                destination.replace(' ','+'),mode,
+    #         #                str(apiKey)
+    #         #                )
+    #         #        )
+    url = ('https://maps.googleapis.com/maps/api/directions/json?units=imperial&origin={}&destination={}&mode={}&key={}'
+        .format(origin.replace(' ','+'),
+                destination.replace(' ','+'),mode_selected,
+                str(apiKey)
+                )
+            )
+    try:
+        response = requests.get(url)
+        resp_json_payload = response.json()
+        step_list = resp_json_payload["routes"][0]["legs"][0]["steps"]
+          
+        for step in step_list:
+            duration = step["duration"]["value"]
+            mode = step["travel_mode"]
+            if mode=="TRANSIT":
+                mode = step["transit_details"]["line"]["vehicle"]["type"]
+            if mode in travel_mode_time:
+                travel_mode_time[mode] = travel_mode_time[mode] + duration
+            else:
+                travel_mode_time[mode] = duration
+    except:
+        print('ERROR: {}, {}'.format(origin, destination))
+    return travel_mode_time
+
+def leg_travel_time(location_list):
+    trips_today = {}
+    for i in range(0,len(location_list)-1):
         origin = location_list[i]
         destination = location_list[i+1]
-        # url = ('https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origin={}&destination={}&mode={}&key={}'
-        #         #        .format(origin.replace(' ','+'),
-        #         #                destination.replace(' ','+'),mode,
-        #         #                str(apiKey)
-        #         #                )
-        #         #        )
-        url = ('https://maps.googleapis.com/maps/api/directions/json?units=imperial&origin={}&destination={}&mode={}&key={}'
-               .format(origin.replace(' ','+'),
-                       destination.replace(' ','+'),mode_selected,
-                       str(apiKey)
-                       )
-               )
-        try:
-            response = requests.get(url)
-            resp_json_payload = response.json()
-            step_list = resp_json_payload["routes"][0]["legs"][0]["steps"]
-            
-            for step in step_list:
-                duration = step["duration"]["value"]
-                mode = step["travel_mode"]
-                if mode=="TRANSIT":
-                    mode = step["transit_details"]["line"]["vehicle"]["type"]
-                if mode in travel_mode_time:
-                    travel_mode_time[mode] = travel_mode_time[mode] + duration
-                else:
-                    travel_mode_time[mode] = duration
-        except:
-            print('ERROR: {}, {}'.format(origin, destination))
-    return travel_mode_time
+        mode_time = get_travel_time(apiKey, origin, destination, mode_selected = 'transit')
+        trips_today[i+1] = {
+            "origin": origin,
+            "destination": destination,
+            "mode_time": mode_time
+        }
+    return trips_today
+    
 
 
 if __name__ == '__main__':
     # get coordinates
     location_list = main()
     print(location_list)
-    travel_times = get_travel_time(apiKey, location_list,mode_selected='transit')
-    for x in travel_times:
-        print(x,travel_times[x])
+    legs = leg_travel_time(location_list)
+    for x in legs:
+        print(x,legs[x])
     # optimization_criteria = drive_time*carbon_footprint
 
