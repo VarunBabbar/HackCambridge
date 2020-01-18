03229437f9e2e0b1d7895f8fcbef5e0620b629fe
Varun Babbar <vb395@cam.ac.uk>
2020-01-18 23:44:59 +0000

    Improved functionality of getLocation.py

--- getLocation.py
+++ getLocation.py
@@ -11,7 +11,19 @@ from google.auth.transport.requests import Request
 
 apiKey = 'AIzaSyA1HfqmvNY_qRHR_aV1JxDDcXKlQMWxuAo'
 
-
+RAIL = 2
+METRO_RAIL = 3
+SUBWAY = 1
+TRAM = 5
+MONORAIL = 1.5
+HEAVY_RAIL = 7
+COMMUTER_TRAIN = 2.3
+HIGH_SPEED_TRAIN = 4.1
+LONG_DISTANCE_TRAIN = 3.4
+BUS = 0.9
+INTERCITY_BUS = 0.8
+TROLLEYBUS = 10.1
+FERRY = 4.5
 
 def get_lat_lng(apiKey, address):
 
@@ -31,8 +43,9 @@ def get_lat_lng(apiKey, address):
 
 def get_travel_time(apiKey,origin,destination,mode_selected): # mode = walking, driving, bicycling, transit
     travel_mode_time = {}
-    # car = 0.525 kg per hour
-    # bus = 0.822 g per km
+    # car = 0.525 kg per hour per person
+    # bus = 0.822 g per km per person
+    # heavy rail = 1 kg per hour per person
     import requests
     time = 0
     carbon_footprint = 0
@@ -80,30 +93,78 @@ def leg_travel_time(location_list):
     return trips_today
     
 
-def getCo2emissions(origin, destination, mode):
-    if mode== 'bicycling':
-        return 0
-    if mode == 'walking':
-        return 0
-    if mode == 'driving':
-        time = get_drive_time(apiKey,origin,destination, mode)
-        timearr = time.split(' ')
-        hours = float(timearr[0])
-        minutes = float(timearr[2])
-        totaltime =  hours + (minutes / 60)
-        co2 = totaltime*0.525
-        return co2, totaltime
+def getCo2emissions(legs):
+    for leg in legs:
+        legx = legs[leg]
+        mode_types = legx['mode_time']
+        railtime = 0
+        meterorailtime = 0
+        subwaytime = 0
+        tramtime = 0
+        monorailtime = 0
+        heavyrailtime = 0
+        commutertraintime = 0
+        highspeedtraintime = 0
+        longdistancetraintime = 0
+        bustime = 0
+        intercitybustime = 0
+        trolleybustime = 0
+        ferrytime = 0
+        for i in list(mode_types.keys()):
+            if i == 'RAIL':
+                railtime += mode_types[i]
+            if i == 'METRO_RAIL':
+                meterorailtime += mode_types[i]
+            if i == 'SUBWAY':
+                subwaytime += mode_types[i]
+            if i == 'TRAM':
+                tramtime += mode_types[i]
+            if i == 'MONORAIL':
+                monorailtime += mode_types[i]
+            if i == 'HEAVY_RAIL':
+                heavyrailtime += mode_types[i]
+            if i == 'COMMUTER_TRAIN':
+                commutertraintime += mode_types[i]
+            if i == 'HIGH_SPEED_TRAIN':
+                highspeedtraintime += mode_types[i]
+            if i == 'LONG_DISTANCE_TRAIN':
+                longdistancetraintime += mode_types[i]
+            if i == 'BUS':
+                bustime += mode_types[i]
+            if i == 'INTERCITY_BUS':
+                intercitybustime += mode_types[i]
+            if i == 'TROLLEYBUS':
+                trolleybustime += mode_types[i]
+            if i == 'FERRY':
+                ferrytime += mode_types[i]
+        railco2 = railtime*RAIL
+        meterorailco2 = meterorailtime*METRO_RAIL
+        subwayco2 = subwaytime*SUBWAY
+        tramco2 = tramtime*METRO_RAIL
+        monorailco2 = monorailtime*METRO_RAIL
+        heavyrailco2 = heavyrailtime*METRO_RAIL
+        commuterrailco2 = commutertraintime*METRO_RAIL
+        highspeedtrainco2 = highspeedtraintime*METRO_RAIL
+        longdistancetrainco2 = longdistancetraintime*METRO_RAIL
+        busco2 = bustime*METRO_RAIL
+        intercitybusco2 = intercitybustime*METRO_RAIL
+        trolleybusco2 = trolleybustime*METRO_RAIL
+        ferryco2 = ferrytime*METRO_RAIL
+        lol = {'RAIL':railco2, 'METRO_RAIL':meterorailco2, 'SUBWAY':subwayco2,'TRAM':tramco2,
+                                  'MONORAIL':monorailco2,'HEAVY_RAIL':heavyrailco2,'COMMUTER_TRAIN':commuterrailco2,
+                                  'HIGH_SPEED_TRAIN':highspeedtrainco2,'LONG_DISTANCE_TRAIN':longdistancetrainco2,'BUS':busco2,'INTERCITY_BUS':intercitybusco2,
+                                  'TROLLEYBUS':trolleybusco2,'FERRY':ferryco2}
+        emissionsdict = {key:val for key,val in lol.items() if val !=0}
+        legx['mode_emissions'] = emissionsdict
     # Get distance, time to between origin and destination
+    return legs
 
 if __name__ == '__main__':
     # get coordinates
     location_list = main()
-    print(location_list)
-    legs = leg_travel_time(location_list)
-    for x in legs:
-        print(x,legs[x])
+    legs = getCo2emissions(leg_travel_time(location_list))
+    for leg in legs:
+        print(leg,legs[leg])
     arr = main()
-    co2,totaltime = getCo2emissions(arr[0], arr[1],'driving')
-    print(str(co2)  + " Kg of Co2 is emitted during the journey from " + arr[0] + " to " + arr[1] + ", which took " + str(totaltime) + " hours")
     # optimization_criteria = drive_time*carbon_footprint
 
