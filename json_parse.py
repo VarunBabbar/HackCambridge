8189ca0e927532a9a7b3b91fe29aff7584aebb23
Jia Wei Kho <jwk33@cam.ac.uk>
2020-01-19 01:19:36 +0000

    JSON Ensure ASCII False

--- json_parse.py
+++ json_parse.py
@@ -3,7 +3,7 @@ import os.path
 
 def json_write(data):
     with open('data.txt', 'w') as outfile:
-        json.dump(data, outfile)
+        json.dump(data, outfile,ensure_ascii=False, indent=4)
 
 def json_read():
     print("hello world")
